8db2c07a264b01564bb085271d54cd0b21937e34
Miguel <miguelhatrick@gmail.com>
2021-09-27 00:33:09 -0300

    cleanuip

--- __manifest__.py
+++ __manifest__.py
@@ -1,10 +1,3 @@
-# Copyright (C) 2004-Today Apertoso NV (<http://www.apertoso.be>)
-# Copyright (C) 2016-Today: La Louve (<http://www.lalouve.net/>)
-# Copyright (C) 2019-Today: Druidoo (<https://www.druidoo.io>)
-# @author: Jos DE GRAEVE (<Jos.DeGraeve@apertoso.be>)
-# @author: Sylvain LE GAL (https://twitter.com/legalsylvain)
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html
-
 {
     'name': 'climbing_gym_pos_customer_required',
     'version': '12.0.1.0.1',
@@ -20,10 +13,6 @@
     ],
     'data': [
         'static/src/xml/templates.xml',
-
-
-        'views/pos_config_view.xml',
-        'views/pos_order_view.xml',
     ],
     'installable': True,
     'qweb': [
--- static/src/js/pos_customer_required.js
+++ static/src/js/pos_customer_required.js
@@ -27,7 +27,7 @@ odoo.define('pos_require_membership', function (require) {
     }
 });
 
-odoo.define('pos_customer_required.pos_customer_required', function (require) {
+odoo.define('climbing_gym_pos_customer_required.pos_customer_required', function (require) {
     "use strict";
 
     var screens = require('point_of_sale.screens');
@@ -37,15 +37,6 @@ odoo.define('pos_customer_required.pos_customer_required', function (require) {
 
     screens.PaymentScreenWidget.include({
         validate_order: function(options) {
-
-            if(this.pos.config.require_customer != 'no'
-                    && !this.pos.get_order().get_client()){
-                this.gui.show_popup('error',{
-                    'title': _t('An anonymous order cannot be confirmed'),
-                    'body':  _t('Please select a customer for this order.'),
-                });
-                return;
-            }
 
             var require_membership_valid = false;
             var require_membership_active = false;
@@ -60,6 +51,15 @@ odoo.define('pos_customer_required.pos_customer_required', function (require) {
                     require_membership_active = true;
                     }
             });
+
+            if((require_membership_valid || require_membership_active)
+                    && !this.pos.get_order().get_client()){
+                this.gui.show_popup('error',{
+                    'title': _t('An anonymous order cannot be confirmed'),
+                    'body':  _t('Please select a customer for this order.'),
+                });
+                return;
+            }
 
             if(require_membership_valid && !this.pos.get_order().get_client().climbing_gym_member_membership_valid){
                 this.gui.show_popup('error',{
@@ -83,28 +83,5 @@ odoo.define('pos_customer_required.pos_customer_required', function (require) {
         }
     });
 
-    /*
-        Because of clientlist screen behaviour, it is not possible to simply
-        use: set_default_screen('clientlist') + remove cancel button on
-        customer screen.
-
-        Instead of,
-        - we overload the function : show_screen(screen_name,params,refresh),
-        - and we replace the required screen by the 'clientlist' screen if the
-        current PoS Order has no Customer.
-    */
-
-    var _show_screen_ = gui.Gui.prototype.show_screen;
-    gui.Gui.prototype.show_screen = function(screen_name, params, refresh){
-        if(this.pos.config.require_customer == 'order'
-                && !this.pos.get_order().get_client()
-                && screen_name != 'clientlist'){
-            // We call first the original screen, to avoid to break the
-            // 'previous screen' mecanism
-            _show_screen_.call(this, screen_name, params, refresh);
-            screen_name = 'clientlist';
-        }
-        _show_screen_.call(this, screen_name, params, refresh);
-    };
 
 });
